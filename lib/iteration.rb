46feea4acc77d9812846c73e5c99543555f83573
Michael Gerland <mr.gerland@gmail.com>
2019-11-06 03:06:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,7 +44,7 @@ def total_even_pairs(src)
       inner_index = 0
       while inner_index < src[index].length do
         if (src[index][inner_index[0]] % 2 == 0) && (src[index][inner_index[1]] % 2 == 0)
-          total += (src[index][inner_index[0]] + src[index][inner_index[0]]
+          total += (src[index][inner_index[0]] + src[index][inner_index[1]]
         end
           inner_index += 1
       end
